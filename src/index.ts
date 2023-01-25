bd359ba96967da2603baed97deb521806403250a
seanlandsman <sean@thelandsmans.com>
2023-01-25 13:27:16 +0000

    AG-7855 add dev modes

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import * as agChartsEnterprise from 'ag-charts-enterprise';
+import { AgChartOptions } from 'ag-charts-enterprise';
+
+const options: AgChartOptions = {
+    container: document.getElementById('myChart'),
+    autoSize: true,
+    title: {
+        text: 'Average expenditure on coffee',
+    },
+    subtitle: {
+        text: 'per person per week in Krakozhia',
+    },
+    data: [
+        {
+            year: '2015',
+            spending: 35,
+        },
+        {
+            year: '2016',
+            spending: 40,
+        },
+        {
+            year: '2017',
+            spending: 43,
+        },
+        {
+            year: '2018',
+            spending: 44,
+        },
+    ],
+    series: [
+        {
+            xKey: 'year',
+            yKey: 'spending',
+        },
+    ],
+};
+
+agChartsEnterprise.AgEnterpriseCharts.create(options);
